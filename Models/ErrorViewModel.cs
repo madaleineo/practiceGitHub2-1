100c2ee81b3a42269b94cfe181d4f35e1d7f5a1e
madaleineo <madaleineosmun1122@gmail.com>
2022-04-04 09:25:36 -0600

    madaleine's commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace practicegit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
